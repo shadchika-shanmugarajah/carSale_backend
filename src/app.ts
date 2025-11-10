2f46c13c0659fa0e9374406b0081d216dfabad8e
Shadchika_Banu <149401217+shadchika-shanmugarajah@users.noreply.github.com>
2025-11-10 18:59:28 +0530

    minar change

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 
 // Configure CORS properly
 app.use(cors({
-  origin: ['https://carsale-56vb.vercel.app/', 'http://localhost:5000'],
+  origin: ['https://carsale-56vb.vercel.app', 'http://localhost:5000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
